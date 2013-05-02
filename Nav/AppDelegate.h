dd27bd3dc81d6da7716b2ec38a41ae254fd4807c
新风作浪 <duxinfeng99@gmail.com>
2013-05-02 09:19:45 +0800

    Initial Commit

---
+++ Nav/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Nav
+//
+//  Created by DolBy on 13-5-2.
+//  Copyright (c) 2013年 Duxinfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
